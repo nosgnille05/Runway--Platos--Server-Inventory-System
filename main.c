f36557c206e8cb2425863cfa8eab16a8fa72154c
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-07-24 14:09:19 +0000

    Inventory Backend

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-
+  int shoes = 0;
+  int shirts = 0;
+  int pants = 0;
+  int total = shoes + shirts + pants;
+  printf("Total %d\n", total);
   return 0; 
   
 }
