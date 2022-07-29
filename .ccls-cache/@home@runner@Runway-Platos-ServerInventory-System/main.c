d29a934b4b537cf60839c37fe88ad706d8c863bc
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-07-29 14:16:12 +0000

    Corner Cases User Feedback

--- .ccls-cache/@home@runner@Runway-Platos-ServerInventory-System/main.c
+++ .ccls-cache/@home@runner@Runway-Platos-ServerInventory-System/main.c
@@ -84,5 +84,11 @@ int main(void) {
   return 0; 
 }
 //Design, Organize, Tech-Stack, API, Implementation
+//Design: What and How?
+//Organize: In what way?
+//Tech-Stack: Flutter/Postgres
+//API: Build it
+//Implementation: Web Application
+
 
 
