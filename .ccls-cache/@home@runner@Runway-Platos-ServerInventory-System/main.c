275589d92bffdb3dad6119363c1bec6ec9aeaa08
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-09-22 00:54:42 +0000

    Add Option for Buying

--- .ccls-cache/@home@runner@Runway-Platos-ServerInventory-System/main.c
+++ .ccls-cache/@home@runner@Runway-Platos-ServerInventory-System/main.c
@@ -64,7 +64,6 @@ int main(void) {
       else
         printf("\nDon't worry! We can fix it now!\n");
     }
-
   } else {
     printf("\nWelcome to (Runway/Platos)! Would you like to buy or sell today? "
            "B or S\n");
