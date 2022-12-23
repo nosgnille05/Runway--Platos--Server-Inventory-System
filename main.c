61799127a78846184175f78acb97394c16d1f0e1
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-12-23 23:03:06 +0000

    Update main

--- main.c
+++ main.c
@@ -29,6 +29,9 @@ int main(void) {
   scanf("%s", admin_or_customer);
   if (strcmp(Admin, admin_or_customer) == 0 ||
       strcmp(admin, admin_or_customer) == 0) {
+
+
+    
     // Get Information
     printf("\nPlease enter the following "
            "information:\n---------------------------------------\n");
