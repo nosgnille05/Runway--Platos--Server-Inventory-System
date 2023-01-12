4a1f9ed4e113d9cf41baf041855c47d6cd200fe4
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2023-01-12 18:33:34 +0000

    Update main

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ int main(void) {
   /*Notes:
     -Design, Organize, Tech-Stack, API, Implementation
     -Design: What and How?
+
+
     -Organize: In what way?
     -Tech-Stack: Flutter/Postgres
     -API: Build it
