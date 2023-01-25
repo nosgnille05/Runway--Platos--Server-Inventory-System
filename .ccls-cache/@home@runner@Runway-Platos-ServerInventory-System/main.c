b792314ca56d2f74e96fb83d254779dbcce3cd58
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2023-01-25 17:27:38 +0000

    Update main

--- .ccls-cache/@home@runner@Runway-Platos-ServerInventory-System/main.c
+++ .ccls-cache/@home@runner@Runway-Platos-ServerInventory-System/main.c
@@ -77,7 +77,6 @@ int main(void) {
       printf("\nPlease enter your First and Last name.\n");
       scanf("%s", seller_fname);
 
-
       scanf("%s", seller_lname);
       printf("\nPlease enter your phone number.\n**Only uses to contact you "
              "when we're done**\n");
